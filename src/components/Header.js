7be26fd020c7a77143fbe1caebed45adcdc53809
Eug-VS <eug-vs@keemail.me>
2019-12-31 16:13:55 +0300

    Use MUI in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,32 @@
 import React from "react";
+import { Tab, Tabs, Typography } from "@material-ui/core";
+import styled from 'styled-components';
 
 const Header = () => (
-  <div>
-    <span> ChronoCube </span>
-    <ul>
-      <li> Home </li>
-      <li> App </li>
-      <li> Scoreboard </li>
-      <li> News </li>
-    </ul>
-  </div>
+  <HeaderWrapper>
+    <Tabs>
+      <Typography variant="h4" id="logo"> ChronoCube </Typography>
+      <Tab label="Home" />
+      <Tab label="App" />
+      <Tab label="Scoreboard" />
+      <Tab label="News" />
+    </Tabs>
+  </HeaderWrapper>
+
 );
+
+const HeaderWrapper = styled.div`
+  border-bottom: 1px solid black;
+  background-color: darkgrey;
+  & .MuiTab-wrapper {
+    padding: 10px;
+  }
+  & .MuiTypography-root {
+    font-weight: bold;
+    padding: 10px;
+    margin-right: 50px;
+    margin-left: 30px;
+  }
+`;
 
 export default Header;

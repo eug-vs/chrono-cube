1d84f837b8577e9b0d453383d0262e2856caa100
Eug-VS <eug-vs@keemail.me>
2020-01-04 13:51:11 +0300

    Add header to SolutionCard, grid the Scoreboard

---
+++ src/SolutionCard/SolutionCard.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import {
+  Typography,
+  Card,
+  CardContent,
+  CardHeader,
+  Avatar,
+  Box,
+} from "@material-ui/core";
+
+import { makeStyles } from "@material-ui/core/styles";
+import TimerIcon from '@material-ui/icons/Timer';
+
+
+const useStyles = makeStyles(theme => ({
+  item: {
+    backgroundColor: theme.palette.secondary.dark,
+    padding: theme.spacing(1),
+
+    '& .MuiBox-root': {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+
+    '& .MuiTypography-h3': {
+      color: theme.palette.orange.main,
+      marginLeft: theme.spacing(1),
+    },
+  },
+}));
+
+const SolutionCard = ({ data }) => {
+  const classes = useStyles();
+  const author = data.author? data.author.toString() : 'anonymous';
+
+  return (
+    <Card elevation={5} className={classes.item}>
+      <CardHeader
+        avatar={
+          author === 'anonymous'?
+            (<Avatar/>)
+            :
+            (<Avatar>{author[0]}</Avatar>)
+        }
+        title={author}
+        subheader="04.01.2020 13:20"
+      />
+      <CardContent>
+        <Box>
+          <TimerIcon/>
+          <Typography variant="h3">
+            { data.result }
+          </Typography>
+        </Box>
+      </CardContent>
+    </Card>
+  )
+};
+
+export default SolutionCard;
--- src/components/Scoreboard/Scoreboard.js
+++ src/components/Scoreboard/Scoreboard.js
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from 'react';
 
-import { Container, Typography } from "@material-ui/core";
+import { Container, Typography, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 import { get } from "../../requests";
-import Solution from "./Solution";
+import SolutionCard from "../../SolutionCard/SolutionCard";
 
+
+const useStyles = makeStyles(theme => ({
+  pageName: {
+    textAlign: 'center',
+    margin: theme.spacing(2),
+  },
+}));
 
 const Scoreboard = () => {
+  const classes = useStyles();
   const [solutions, setSolutions] = useState([]);
 
   const updateSolutions = async () => {
@@ -20,9 +29,19 @@ const Scoreboard = () => {
 
 
   return (
-    <Container maxWidth="sm">
-      <Typography variant="h3" style={{textAlign: 'center'}}>Scoreboard</Typography>
-      {solutions.map(solution => (<Solution solution={solution}/>))}
+    <Container maxWidth="xs">
+      <Typography variant="h3" className={classes.pageName}>
+        Scoreboard
+      </Typography>
+      <Grid container direction="column" justify="center" spacing="3">
+        {
+          solutions.map(solution => (
+            <Grid item>
+              <SolutionCard data={solution}/>
+            </Grid>
+          ))
+        }
+      </Grid>
     </Container>
   );
 };

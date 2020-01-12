e1bf09bbd748f99f7693d26d855306e574c8919f
Eug-VS <eug-vs@keemail.me>
2020-01-12 18:20:01 +0300

    Change baseUrl to public API url

--- src/requests.js
+++ src/requests.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:8000/';
+const baseUrl = 'http://eugvs.pythonanywhere.com/';
 const baseApiUrl = baseUrl + 'api/';
 
 export const get = (url) => {

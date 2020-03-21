2ca9cbb36e25e280a1b26c9dc03eaea30e1dcc9d
eug-vs <eug-vs@keemail.me>
2020-03-21 15:20:41 +0300

    refactor: improve requests.ts

--- src/requests.ts
+++ src/requests.ts
@@ -3,22 +3,9 @@ import axios, { AxiosResponse } from 'axios';
 const baseUrl = 'https://eugvs.pythonanywhere.com/';
 const baseApiUrl = baseUrl + 'api/';
 
-export const get = (url: string): Promise<AxiosResponse> => {
-  return axios.get(
-    baseApiUrl + url,
-  );
-};
+export const get = (url: string): Promise<AxiosResponse> => axios.get(baseApiUrl + url);
 
-export const post = (url: string, data: object): Promise<AxiosResponse> => {
-  return axios.post(
-    baseApiUrl + url,
-    data
-  );
-};
+export const del = (url: string): Promise<AxiosResponse> => axios.delete(baseApiUrl + url);
 
-export const del = (url: string, data: object): Promise<AxiosResponse> => {
-  return axios.delete(
-    baseApiUrl + url,
-    data
-  )
-};
+export const post = (url: string, data: object): Promise<AxiosResponse> => axios.post(baseApiUrl + url, data);
+

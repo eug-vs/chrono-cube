e32c8be0a4b7f135c01449c3603da347728819a8
eug-vs <eug-vs@keemail.me>
2020-03-21 14:35:19 +0300

    chore: migrate requests.ts

--- src/requests.ts
+++ src/requests.ts
@@ -3,20 +3,20 @@ import axios from 'axios'
 const baseUrl = 'https://eugvs.pythonanywhere.com/';
 const baseApiUrl = baseUrl + 'api/';
 
-export const get = (url) => {
+export const get = (url: string) => {
   return axios.get(
     baseApiUrl + url,
   );
 };
 
-export const post = (url, data) => {
+export const post = (url: string, data: object) => {
   return axios.post(
     baseApiUrl + url,
     data
   );
 };
 
-export const del = (url, data) => {
+export const del = (url: string, data: object) => {
   return axios.delete(
     baseApiUrl + url,
     data
